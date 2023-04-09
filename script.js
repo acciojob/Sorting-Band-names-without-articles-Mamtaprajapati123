f84c66f129d25db5d58cdf47fe66c23b10b04364
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-09 18:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+const excludedWords = ["a", "an", "the"];
+
+// Function to remove excluded words from band names
+function removeExcludedWords(bandName) {
+  return bandName.split(" ").filter((word) => !excludedWords.includes(word.toLowerCase())).join(" ");
+}
+
+// Sort band names in lexicographic order (excluding excluded words)
+const sortedBandNames = bandNames.map((name) => removeExcludedWords(name)).sort();
+
+// Add sorted band names to list
+const bandList = document.getElementById("band");
+sortedBandNames.forEach((name) => {
+  const li = document.createElement("li");
+  li.textContent = name;
+  bandList.appendChild(li);
+});
 
