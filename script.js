8fd8416b233412bfe3c0c91e823bb68a2bbbc0d6
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-16 16:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Array of band names
-const bands = ['The Beatles', 'Radiohead', 'Led Zeppelin', 'Aerosmith', 'The Who', 'Pink Floyd'];
-
-// Function to strip articles from band names
-function strip(bandName) {
-  return bandName.replace(/^(a |an |the )/i, '').trim();
+function strip(article) {
+  return article.replace(/^(a |an |the )/i, '').trim();
 }
 
-// Sort the band names
 const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-// Select the ul element
 const ul = document.querySelector('#bands');
 
-// Create and add li elements for each band name
-ul.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
