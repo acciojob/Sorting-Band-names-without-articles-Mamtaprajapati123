7a46635596453f68bde3dc1c95c98c87ef43a79e
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-05-13 14:53:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const bands = ['The Rolling Stones', 'Led Zeppelin', 'The Who', 'Pink Floyd', 'The Beatles', 'Aerosmith'];
-
-function strip(article) {
-  return article.replace(/^(a |an |the )/i, '').trim();
+function removeArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
 }
 
-const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
-
-const ul = document.querySelector('#bands');
-
-sortedBands.forEach(band => {
-  const li = document.createElement('li');
-  li.textContent = band;
-  ul.appendChild(li);
+// sort the band names in lexicographic order without articles
+bandNames.sort(function(a, b) {
+  return removeArticles(a) > removeArticles(b) ? 1 : -1;
 });
+
+// get the UL element
+let bandList = document.getElementById('band');
+
+// create a list item for each band name and append it to the UL element
+for (let i = 0; i < bandNames.length; i++) {
+  let li = document.createElement('li');
+  li.textContent = bandNames[i];
+  bandList.appendChild(li);
+}
+In this code, we first define an array of band names. Then, we define a function called removeArticles that takes a band name as input and returns the name with any article removed (i.e., "The ", "A ", or "An "). We then use this function to sort the band names in lexicographic order without articles.
+
+Next, we get a reference to the ul element with an ID of "band" and loop through the sorted band names. For each band name, we create a new li element, set its textContent property to the band name, and append it to the ul element.
+
+The resulting HTML would look something like this:
+
+html
+Copy code
+<ul id="band">
+  <li>Led Zeppelin</li>
+  <li>Nirvana</li>
+  <li>Pink Floyd</li>
+  <li>The Beatles</li>
+  <li>Rolling Stones</li>
+</ul>
+
+
+
+
+
